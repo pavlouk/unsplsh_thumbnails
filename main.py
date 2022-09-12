42f03ea52b01427182dcb53caf82f2a3e90f7e7a
ploukareas <ploukareas@gmail.com>
2022-09-12 15:59:17 +0300

    Revert "Revert "constructed unsplash url""
    This reverts commit 3214d4fd3b8e987ac3dbe6fdfb5bc92b0e9993c9.
    
    fixing wrong import issue

--- main.py
+++ main.py
@@ -17,4 +17,10 @@ def hello():
 
 @app.get("/search/{query}")
 def search(query: str, orientation: Optional[str] = None, color: Optional[str] = None):
-    return {"some": "thing"}
+    search_url = SEARCH_URL + f"?query={query}"
+    if orientation is not None:
+        search_url += f"&orientation={orientation}"
+    if color is not None:
+        search_url += f"&color={color}"
+    search_url += ACCESS_KEY
+    return {"Search using": search_url}
