573f1f4ee13bfe5afe74b1c5f3a191a5339ec1c1
ploukareas <ploukareas@gmail.com>
2022-09-14 16:50:49 +0300

    configured enums to search request body

--- thumb_app/schemas.py
+++ thumb_app/schemas.py
@@ -1,7 +1,11 @@
+from typing import Optional
+
 from pydantic import BaseModel
+
+from .enums import ColorEnum, OrientationEnum
 
 
 class SearchBase(BaseModel):
     query: str
-    color: str
-    orientation: str
+    color: Optional[ColorEnum] = None
+    orientation: Optional[OrientationEnum] = None
