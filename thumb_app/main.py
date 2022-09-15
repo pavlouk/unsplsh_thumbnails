af64447316c9697cb48cbb69281416192d6138e1
ploukareas <ploukareas@gmail.com>
2022-09-15 10:12:01 +0300

    added query url build

--- thumb_app/main.py
+++ thumb_app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+import validators
+from fastapi import FastAPI, HTTPException
+
+from . import schemas
+from .external import unsplash_builder
 
 app = FastAPI()
 
@@ -6,3 +10,14 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return "Welcome to the unofficial Unsplash thumbnails API!"
+
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+
+@app.post("/search")
+def image_search(search: schemas.SearchBase):
+    if not validators.url(unsplash_builder(search)):  # type: ignore
+        raise_bad_request(message="Your provided query is not valid")
+    return f"TODO: Create database entry for: {search.query}"
