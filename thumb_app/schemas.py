b6940e792469a718c0320bb9215c992a5508c8b1
ploukareas <ploukareas@gmail.com>
2022-09-15 10:46:45 +0300

    update response schemas

--- thumb_app/schemas.py
+++ thumb_app/schemas.py
@@ -1,3 +1,4 @@
+from re import L
 from typing import Optional
 
 from pydantic import BaseModel
@@ -9,3 +10,16 @@ class SearchBase(BaseModel):
     query: str
     color: Optional[ColorEnum] = None
     orientation: Optional[OrientationEnum] = None
+
+
+class Image(SearchBase):
+    is_active: bool
+    clicks: int
+
+    class Config:
+        orm_mode = True
+
+
+class ImageInfo(Image):
+    url: str
+    admin_url: str
