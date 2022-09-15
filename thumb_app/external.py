514833ad56fcae0c582e7f3e092ddb5c5f602030
ploukareas <ploukareas@gmail.com>
2022-09-15 10:50:00 +0300

    fetched unsplash image and create thumbnail

--- thumb_app/external.py
+++ thumb_app/external.py
@@ -1,3 +1,8 @@
+from io import BytesIO
+
+import httpx
+from PIL import Image
+
 from . import schemas
 from .config import get_settings
 
@@ -10,3 +15,24 @@ def unsplash_builder(search: schemas.SearchBase) -> str:
         search_url += f"&color={search.color.value}"
     search_url += f"&client_id={get_settings().unsplash_key}"
     return search_url
+
+
+def create_thumbnail(buffered_image: BytesIO):
+    image = Image.open(buffered_image)
+    return image.thumbnail(
+        size=(200, image.height * 200 // image.width), resample=Image.Resampling.LANCZOS
+    )
+
+
+async def fetch_image(search_url: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(search_url)
+        response.raise_for_status()
+        response_body = response.json()
+        search_results = response_body.get("results", {})
+        urls = search_results[0].get("urls", {})
+        raw_url = urls.get("small", "Error")
+        image_response = await client.get(raw_url)
+        response.raise_for_status()
+        image_buffer = BytesIO(image_response.content)
+        return create_thumbnail(image_buffer)
