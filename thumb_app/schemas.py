4e71c599603b59b5fdc764f1ea3aa17c5d4d8ffa
ploukareas <ploukareas@gmail.com>
2022-09-15 16:01:03 +0300

    keep input request body sschema

--- thumb_app/schemas.py
+++ thumb_app/schemas.py
@@ -1,4 +1,3 @@
-from re import L
 from typing import Optional
 
 from pydantic import BaseModel
@@ -10,16 +9,3 @@ class SearchBase(BaseModel):
     query: str
     color: Optional[ColorEnum] = None
     orientation: Optional[OrientationEnum] = None
-
-
-class Image(SearchBase):
-    is_active: bool
-    clicks: int
-
-    class Config:
-        orm_mode = True
-
-
-class ImageInfo(Image):
-    url: str
-    admin_url: str
